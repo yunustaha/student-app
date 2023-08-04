cdf9f5874ee9dddfb90fad0a407d0378b8a3ad32
yunustahayilmaz <yunus@mobimyazilim.com>
2023-08-04 12:42:02 +0300

    student page responsive design added.

--- src/app/(pages)/students/page.tsx
+++ src/app/(pages)/students/page.tsx
@@ -16,15 +16,15 @@ const Students = () => {
 
     return (
         <div className='flex flex-col bg-student-gray-background h-full w-full px-8 py-5'>
-            <div className='flex justify-between items-center'>
-                <div className='text-2xl font-bold'>Students List</div>
+            <div className='flex flex-col lg:flex-row lg:justify-between items-center'>
+                <div className='text-2xl mb-4 lg:mb-0 lg:text-2xl font-bold'>Students List</div>
                 <div className='flex gap-6 items-center'>
-                    <div className='h-[37px] w-[212px]'>
+                    <div className='lg:w-[212px]'>
                         <input className='student-input' onChange={(e) => setGridVal((current) => ({ ...current, search: e.target.value }))} placeholder='Search...'></input>
                     </div>
-                    <div className='h-[44px] w-[199px]'>
+                    <div className='lg:w-[199px]'>
                         <button onClick={() => setGridVal((current) => ({ ...current, newStudentOpen: true }))}
-                            className='student-button '>ADD NEW STUDENT</button>
+                            className='student-button '>ADD STUDENT</button>
                     </div>
                 </div>
             </div>
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -3,15 +3,17 @@ import GridRows from './GridRows'
 
 const Grid = () => {
     return (
-        <div className='flex flex-col'>
-            <div className='grid grid-cols-7 text-student-gray-field text-xs font-semibold justify-around px-3'>
-                <div className='col-start-2'>Name</div>
-                <div>Email</div>
-                <div>Phone</div>
-                <div>Website</div>
-                <div>Company Name</div>
+        <div className='w-full overflow-x-scroll'>
+            <div className='flex flex-col min-w-[1000px]'>
+                <div className='grid grid-cols-7 text-student-gray-field text-xs font-semibold justify-around px-3'>
+                    <div className='col-start-2'>Name</div>
+                    <div>Email</div>
+                    <div>Phone</div>
+                    <div>Website</div>
+                    <div>Company Name</div>
+                </div>
+                <GridRows />
             </div>
-            <GridRows />
         </div>
     )
 }
